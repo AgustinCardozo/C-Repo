b15acd097697c0fccc99e0b7929307be268957ca
Juan-Polito <jpolito@frba.utn.edu.ar>
2023-06-14 11:16:04 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
+
+git_USUARIO = "USUARIO"
+git_PASSWORD = "TOKEN"
+
 testfunction(){
-   echo "My first function"
+
+   echo "Ingresa USUARIO:"
+   read git_USUARIO
+
+   echo "Ingresa PASSWORD/TOKEN:"
+   read git_PASSWORD
+
+   cd $1
+   git install https://github.com/sisoputnfrba/so-commons-library.git $1
+   git_USUARIO $1
+   git_PASSWORD $1
+   echo "Se instalaron las commons"
+
+   cd $1
+   git install https://github.com/sisoputnfrba/tp-2023-1c-EstaEsLaVencida.git $1
+   git_USUARIO $1
+   git_PASSWORD $1
+   echo "Se instalo nuestro TP"
+
+   cd so-commons-library/ $1
+   make all $1
+   echo "Se generaron las estructuras de las COMMONS"
+
+
    cd $1
    cd tp-2023-1c-EstaEsLaVencida/compartido/Debug/ $1
    make all $1
+   cd .. $1
+   cd src/ $1
+   make install $1
+   utnso $1
+   echo "Se generaron las estrucuras de COMPARTIDO"
 
    cd $1
    cd tp-2023-1c-EstaEsLaVencida/memoria/Debug/ $1
@@ -14,15 +46,17 @@ testfunction(){
    make all $1
 
    cd $1
-   cd tp-2023-1c-EstaEsLaVencida/kernel/Debug/ $1
+   cd tp-2023-1c-EstaEsLaVencida/fileSystem/Debug/ $1
    make all $1
 
    cd $1
-   cd tp-2023-1c-EstaEsLaVencida/fileSystem/Debug/ $1
+   cd tp-2023-1c-EstaEsLaVencida/kernel/Debug/ $1
    make all $1
 
    cd $1
    cd tp-2023-1c-EstaEsLaVencida/consola/Debug/ $1
    make all $1
+
+   echo "todo listo para ejecucion"
 }
 testfunction
